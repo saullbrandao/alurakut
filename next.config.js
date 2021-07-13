b3af1c59f865961b2e81526fa031327aa8173a6a
Saull Brandão <saull@outlook.com>
2021-07-13 09:28:32 -0300

    aula 2

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['github.com'],
+    domains: ['github.com', 'place-hold.it', 'alurakut.vercel.app'],
   },
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,10 @@
 import { MainGrid } from '../src/components/MainGrid'
 import { Box } from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import {
+  AlurakutMenu,
+  AlurakutProfileSidebarMenuDefault,
+  OrkutNostalgicIconSet,
+} from '../src/lib/AlurakutCommons'
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
@@ -13,19 +17,35 @@ const ProfileImage = styled(Image)`
 
 function ProfileSidebar({ githubUser }) {
   return (
-    <Box>
+    <Box as="aside">
       <ProfileImage
         src={`https://github.com/${githubUser}.png`}
         alt="Profile picture"
         width="128"
         height="128"
       />
+      <hr />
+      <p>
+        <a href={`https://github.com/${githubUser}`} className="boxLink">
+          @{githubUser}
+        </a>
+      </p>
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
 
 export default function Home() {
-  const githubUser = 'saullbrandao'
+  const githubUser = 'torvalds'
+  const [communities, setCommunities] = useState([
+    {
+      id: 'gergerherthrthrthjrthjrtyrtyjtyjktym514614',
+      title: 'Eu odeio acordar cedo',
+      image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg',
+    },
+  ])
   const [followers, setFollowers] = useState([])
 
   useEffect(() => {
@@ -44,6 +64,17 @@ export default function Home() {
     getFollowers(githubUser)
   }, [githubUser])
 
+  function handleCreateCommunity(event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const community = {
+      id: new Date().toISOString(),
+      title: formData.get('title'),
+      image: formData.get('image'),
+    }
+    setCommunities([...communities, community])
+  }
+
   return (
     <>
       <AlurakutMenu githubUser={githubUser} />
@@ -55,6 +86,28 @@ export default function Home() {
           <Box>
             <h1 className="title">Bem vindo(a)</h1>
             <OrkutNostalgicIconSet confiavel={3} legal={2} sexy={1} />
+          </Box>
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form onSubmit={handleCreateCommunity}>
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma URL para usar de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usar de capa"
+                />
+              </div>
+
+              <button>Criar comunidade</button>
+            </form>
           </Box>
         </div>
         <div
@@ -85,7 +138,28 @@ export default function Home() {
               })}
             </ul>
           </ProfileRelationsBoxWrapper>
-          <Box>Comunidades</Box>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">Comunidades ({communities.length})</h2>
+            <ul>
+              {communities.map(({ title, image, id }, index) => {
+                if (index <= 5) {
+                  return (
+                    <li key={id}>
+                      <a href={'#'}>
+                        <Image
+                          src={image}
+                          alt="Community picture"
+                          layout="fill"
+                          objectFit="cover"
+                        />
+                        <span>{title}</span>
+                      </a>
+                    </li>
+                  )
+                }
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
         </div>
       </MainGrid>
     </>
